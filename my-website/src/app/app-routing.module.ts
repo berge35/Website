938e331c98ab39059b1a4ab75e2dd8dbbafd2530
Christopher Elliott <35berge35@gmail.com>
2018-03-12 23:45:23 -0400

    Added basic navbar Added jumbotrop with stock image Added profile pic and text box Added about and projects components

--- my-website/src/app/app-routing.module.ts
+++ my-website/src/app/app-routing.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component'
+import { ProjectsComponent} from './projects/projects.component';
+import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
     
-  }
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+    
+  },
+  {
+    path: 'about-me',
+    component: AboutComponent
+    
+  },
 
 ];
 
--- my-website/src/app/app.module.ts
+++ my-website/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    ProjectsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
